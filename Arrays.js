61418979e25d8081cf861149c6051a298475738f
dett0l <105518065+dett0ll@users.noreply.github.com>
2025-08-05 20:39:50 +0530

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+//creating array from String
+let s = 'This is a string';
+var arr= s.split('');
+console.log(arr); 
+/*[
+  'T', 'h', 'i', 's',
+  ' ', 'i', 's', ' ',
+  'a', ' ', 's', 't',
+  'r', 'i', 'n', 'g'
+]*/
+var arr2= s.split(' ');//add a space
+console.log(arr2);
+[ 'This', 'is', 'a', 'string' ]
