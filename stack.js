ed038257573fc025894f73b0e19698ef644460b8
dett0l <105518065+dett0ll@users.noreply.github.com>
2025-08-21 20:40:24 +0530

    Update stack.js

--- stack.js
+++ stack.js
@@ -26,3 +26,49 @@ console.log(firstStack.peek());//a
 firstStack.push('b');
 console.log(firstStack.dataStore);//[ 'a', 'b' ]
 console.log(firstStack.peek());//b
+
+//palindrome checker
+function Stack() {
+ this.dataStore = [];
+ this.top = 0; //keeps track of the top of stack
+ this.push = push; //add elements to stack
+ this.pop = pop; //remove from stack
+ this.peek = peek;//returns value stored at top of stack
+ this.length = length;
+}
+function push(element){
+  this.dataStore[this.top++] = element;
+}
+function pop(){
+  return this.dataStore[--this.top];
+}
+function peek(){
+ return this.dataStore[this.top-1];
+}
+function length(){
+  return this.top;
+}
+function isPalindrome(word){
+  for (let i=0; i<=word.length-1;i++){
+    s.push(word[i])
+  }
+  //console.log(s.dataStore);
+  let rword = ''
+  while(s.length()>=1){
+    rword += s.pop()
+  }
+  //console.log(rword);
+  if(word === rword){
+    return true;
+  }
+}
+
+var s = new Stack();
+let word = "racecar";
+
+if (isPalindrome(word)){
+  console.log(`${word} is a palindrome`);
+}else{
+  console.log(`${word} is not a palindrome`);
+}
+
